aade1efc555d53bf064877bda4f3a3b5bbb34b95
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-04-25 08:32:26 +0300

    i18n: add some localication strings

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,9 +1,11 @@
 import en from "./en.json?raw";
 import bg from "./bg.json?raw";
+import ja from "./ja.json?raw";
 
 export const languages = {
   en: "English",
   bg: "Български",
+  ja: "日本語",
 };
 
 export const defaultLang = "en";
@@ -11,6 +13,7 @@ export const defaultLang = "en";
 export const ui = {
   en: JSON.parse(en),
   bg: JSON.parse(bg),
+  ja: JSON.parse(ja),
 } as const;
 
 export type Lang = keyof typeof ui;
